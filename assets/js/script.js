486f752ce9f6556b9f0f26706339c68367019e0d
chadkraus87 <127354484+chadkraus87@users.noreply.github.com>
2023-04-04 19:17:21 -0500

    Started building HTML, added assets folders, CSS, JavaScript outlines.

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
+
+var startBtn = document.querySelector("#start");
+
+var Timer = document.querySelector("#Timer");
+var seconds = 10;
+startBtn.addEventListener("click", function () {
+  console.log("Start button clicked.");
+  startTimer();
+});
+
+function startTimer() {
+  var clock = setInterval(function () {
+    seconds--;
+    Timer.textContent = seconds + " left on the clock.";
+  }, 1000);
+}
