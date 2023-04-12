92f8932c647e07cde78770f137ba655c97e44e51
chadkraus87 <127354484+chadkraus87@users.noreply.github.com>
2023-04-11 20:46:44 -0500

    Removed unnecessary code.

--- assets/js/highscoresjs.js
+++ assets/js/highscoresjs.js
@@ -3,16 +3,6 @@ if (highScores.length > 0) {
     highScores.forEach(function (score) {
         console.log(score);
         console.log(score.initials);
-//         var tr = document.createElement("tr");
-// var initialstd = document.createElement("td");
-// var scoretd = document.createElement("td");
-// var score = document.createElement("p");
-// var initials = document.createElement("p");
-// initials.textContent = `${score.initials}`;
-// initialstd.appendChild(initials);
-// score.innerText = score.score;
-// scoretd.appendChild(score);
-// tr.append(initialstd, scoretd);
 
 var li = document.createElement("li");
 li.textContent = `${score.initials} - ${score.score}`;
